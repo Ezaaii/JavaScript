8c082fe2afd2cf424e91bcf2234138349d5bc03c
Unknown <n.vanroost@gmail.com>
2017-10-16 16:57:05 +0200

    Update java

--- JavaScript_exo6/app.js
+++ JavaScript_exo6/app.js
@@ -1,12 +1,29 @@
 //demande le mot à deviner//
-var str = prompt("Choisissez un mot.");
+var mot = prompt("Choisissez un mot.");
 //séparateur de lettres//
-let mot=[];
-mot.push(str);
-console.log(mot)
-var lettres = str.match(/.{1,1}/g);
+let str1=[];
+str1.push(mot);
+console.log(str1);
+var lettres = mot.match(/.{1,1}/g);
 console.log(lettres);
 //affiche les nombre de lettres à deviner//
-var bite = new Array(lettres.length);
-console.log(bite);
+var long = new Array(lettres.length);
+console.log(long);
 //tableau de lettres devinées//
+let str2=[];
+
+if (str1.length=0){
+  console.log("Bravo, vous avez trouvé "+mot+".")
+  } else {
+    for (var i=0; i<9; i++){
+      var mot2 = prompt("Nouvelle lettre:");
+      str2.push(mot2);
+      console.log(str2);
+      if (mot2 == mot[i]){
+        console.log("gg!");
+      } else {
+          console.log("raté");
+          str2.pop(mot2[i]);
+      }
+    }
+}
