960600c7bf0856c7ec87bb774d09c50c182dddff
Unknown <n.vanroost@gmail.com>
2017-10-18 09:29:41 +0200

    Final Exo6 (pendu jouable et opti)

--- JavaScript_exo6/app.js
+++ JavaScript_exo6/app.js
@@ -1,29 +1,45 @@
 //demande le mot à deviner//
 var mot = prompt("Choisissez un mot.");
-//séparateur de lettres//
-let str1=[];
-str1.push(mot);
-console.log(str1);
-var lettres = mot.match(/.{1,1}/g);
-console.log(lettres);
+//TABLEAU 1 (+ séparateur de lettres)//
+let str1 = mot.match(/.{1,1}/g);
 //affiche les nombre de lettres à deviner//
-var long = new Array(lettres.length);
+var long = new Array(str1.length);
 console.log(long);
-//tableau de lettres devinées//
+//TABLEAU 2 (vide)//
 let str2=[];
-
-if (str1.length=0){
-  console.log("Bravo, vous avez trouvé "+mot+".")
-  } else {
-    for (var i=0; i<9; i++){
-      var mot2 = prompt("Nouvelle lettre:");
-      str2.push(mot2);
-      console.log(str2);
-      if (mot2 == mot[i]){
-        console.log("gg!");
-      } else {
-          console.log("raté");
-          str2.pop(mot2[i]);
-      }
+//fin du programme équivaux au nombre de carractères à deviner//
+var fin = str1.length;
+//var d
+var erreur=0;
+//boucle secondaire : guess letter//
+function guess(){
+  var mot2 = prompt("Essayez une lettre (Vous avez fait "+erreur+" erreurs)");
+  var correction = false;
+  for (var i=0; i<str1.length;i++){
+    if (mot2==str1[i]){
+      str2[i]=(mot2);
+      fin--;
+      correction=true;
     }
+  }
+  if (correction){
+    alert("GG!");
+    document.getElementById("string2").innerHTML=(str2);
+    document.write(mot2);
+  } else {
+    alert("raté!");
+    document.write(mot2);
+    erreur++;
+  }
+  console.log(str2);
+}
+//boucle principale//
+while (fin>0){
+  guess();
+  if(fin==0){
+    alert("Bravo, vous avez trouvé le mot [ "+mot+" ]!");
+  } else if (erreur>4){
+    alert("Désolé vous avez perdu. Le mot était [ "+mot+" ]");
+    fin=0;
+  }
 }
